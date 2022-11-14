2b3a9b076fbb928d01cd987030f85f2da60973c0
srikant <raos17@medtronic.com>
2022-11-14 20:52:23 +0530

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Admin on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
