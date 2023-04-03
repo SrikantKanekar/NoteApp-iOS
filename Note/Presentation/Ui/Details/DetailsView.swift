45365ab5da6cb89469a5cea349a5ba0cc2265ed9
srikant <srikant.kanekar@medtronic.com>
2023-04-03 09:28:00 +0530

    add details actions

--- Note/Presentation/Ui/Details/DetailsView.swift
+++ Note/Presentation/Ui/Details/DetailsView.swift
@@ -11,6 +11,10 @@ struct DetailsView: View {
     @State private var titleText: String = "Title"
     @State private var bodyText: String = "This is some editable text..."
     
+    @State private var showingAddOptions: Bool = false
+    @State private var showingColorOptions: Bool = false
+    @State private var showingMoreOptions: Bool = false
+    
     var body: some View {
         VStack {
             TextField("Title", text: $titleText)
@@ -20,10 +24,69 @@ struct DetailsView: View {
         .padding()
         .navigationBarTitle("", displayMode: .inline)
         .toolbar {
-            Button {
-                
-            } label: {
-                Label("save", systemImage: "pencil")
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Label("pin", systemImage: "pin")
+                }
+                Button {
+                    
+                } label: {
+                    Label("reminder", systemImage: "alarm")
+                }
+                Button {
+                    
+                } label: {
+                    Label("archive", systemImage: "archivebox")
+                }
+            }
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button(
+                    action: {
+                        showingAddOptions.toggle()
+                    },
+                    label: {
+                        Label("Add", systemImage: "plus.app")
+                    }
+                )
+                .confirmationDialog("Add", isPresented: $showingAddOptions, titleVisibility: .hidden) {
+                    Button("Take photo") {}
+                    Button("Add image") {}
+                    Button("Drawing") {}
+                    Button("Recording") {}
+                    Button("Tick boxes") {}
+                }
+                Button(
+                    action: {
+                        showingColorOptions.toggle()
+                    },
+                    label: {
+                        Label("Color", systemImage: "paintpalette")
+                    }
+                )
+                .confirmationDialog("color", isPresented: $showingColorOptions, titleVisibility: .hidden) {
+                    Button("Colour") {}
+                    Button("Background") {}
+                }
+                Button(
+                    action: {
+                        showingMoreOptions.toggle()
+                    },
+                    label: {
+                        Label("More", systemImage: "ellipsis.circle")
+                    }
+                )
+                .confirmationDialog("more", isPresented: $showingMoreOptions, titleVisibility: .hidden) {
+                    Button("Delete") {}
+                    Button("Make a copy") {}
+                    Button("Send") {}
+                    Button("Collaborator") {}
+                    Button("Labels") {}
+                }
+            }
+            ToolbarItemGroup(placement: .status) {
+                Text("Edited 12 Jan 2023")
             }
         }
     }
